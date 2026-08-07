1df144412b14ce7fac64d45afc85cfa728b358ce
lcnfreitas <luciano.freitas@nutanix.com>
2026-08-07 04:12:36 +0000

    Ajusta imagem docker para lbf78 e adiciona k8s manifests

--- config/config.js
+++ config/config.js
@@ -1,10 +1,9 @@
-
 // Supported dialects are: mssql, mariadb, mysql, postgres, and sqlite
 module.exports = {
-    DB_NAME: 'REPLACE_DB_NAME',
-    DB_HOST_ADDRESS: 'REPLACE_DB_HOST_ADDRESS',
-    DB_DIALECT: 'REPLACE_DB_DIALECT',
-    DB_DOMAIN_NAME: 'REPLACE_DB_DOMAIN_NAME',
-    DB_USER_NAME: 'REPLACE_DB_USER_NAME', // default: "Administrator" for MSSQL and "root" for all others
-    DB_PASSWORD: 'REPLACE_DB_PASSWORD' // only applicable to MS SQL connections
+  DB_NAME: process.env.DB_NAME || 'fiestadb',
+  DB_HOST_ADDRESS: process.env.DB_HOST || '127.0.0.1',
+  DB_DIALECT: process.env.DB_DIALECT || 'mysql',
+  DB_DOMAIN_NAME: process.env.DB_DOMAIN_NAME || '',
+  DB_USER_NAME: process.env.DB_USER || 'root',
+  DB_PASSWORD: process.env.DB_PASSWORD || 'nutanix/4u'
 }
